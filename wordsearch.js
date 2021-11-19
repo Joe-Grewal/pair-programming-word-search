6f4cbbe933ebde200572e0ea160e73701f134800
Jorawer Grewal <joegrewal20@gmail.com>
2021-11-19 10:12:20 -0800

    Added transpose function locally so others can run it

--- wordsearch.js
+++ wordsearch.js
@@ -1,70 +1,49 @@
-const transpose = require('../../../../joegrewal/w4/matrix_transposition')
+const transpose = function(matrix) {
+    const newArray = [];
+  
+    if (matrix.length === 1) {
+      for (let j = 0; j < matrix[0].length; j++) {
+        let original = matrix[0][j];
+        newArray.push([original]);
+      }
+    } else {
+      for (let i = 0; i < matrix[0].length; i++) {
+        newArray.push([]);
+      } 
+      for (let row = 0; row < matrix.length; row++) {
+        for (let column = 0; column < matrix[row].length; column++) {
+          let original = matrix[row][column];
+          newArray[column].push(original);
+        } 
+      }
+    }
+    return newArray;
+  };
 
 // Input is 2d array of letters and a word
 // check to see if the word exists either horizontally or vertically
 // PSEUDO CODE
 // iterate through horizontally
 //  check for the string
-// transpose matrix
+// transpose matrix 
 //  iterate again and check again
 
 const wordSearch = (letters, word) => { 
     if (letters.length === 0) {
-        // throw new Error('This array is empty');
         return "This array is empty";
     }
     if (typeof(word) !== 'string') {
         return "This word is not a string";
     }
     const horizontalJoin = letters.map(ls => ls.join(''));
-    // console.log(horizontalJoin);
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
     const verticalJoin = transpose(letters).map(ls => ls.join(''));
-    // console.log(verticalJoin);
     for (l of verticalJoin) {
         if (l.includes(word)) return true
     }
      return false;
 };
-
-// console.log(wordSearch([
-//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-//   ], 'SEINFXELD'), "should be False");
-
-// console.log(wordSearch([
-//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-//   ], 'SEINFELD'), "should be True");
-
-//   console.log(wordSearch([
-//     ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-//     ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-//     ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-//     ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-//     ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-//     ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
-//     ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
-//     ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-//   ], 'WEFMHFBDZ'), "should be True");
-
-//   console.log(wordSearch([], "WORD"), "?");
 
 module.exports = wordSearch;
